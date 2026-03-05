b36a3ddb11f414eaee40fb1e0372ff4d7f85f7dd
ankush-vaishvitechnology <ankush@vaishvitechnology.com>
2026-03-05 17:48:58 +0530

    LunarTaxSeeder added for default data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,12 +8,22 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
      */
     public function run(): void
     {
-        $this->call(SuperAdminSeeder::class);
+        // Core Lunar setup (Currency, Channel, Language, TaxClass)
         $this->call(LunarSetupSeeder::class);
+
+        // Tax configuration (TaxZone, TaxRate, TaxRateAmount)
+        $this->call(LunarTaxSeeder::class);
+
+        // Customer groups for pricing tiers
+        $this->call(CustomerGroupSeeder::class);
+
+        // Admin user
+        $this->call(SuperAdminSeeder::class);
+
+        // Product organization
         $this->call(TagSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ProductCategorySeeder::class);
